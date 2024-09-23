d0c1098329a0bbfa6846486cc8efb37af1d6607f
wnsgks3153 <72612538+wnsgks3153@users.noreply.github.com>
2024-09-23 14:16:30 +0900

    Title: 출석 부르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240923/출석 부르기/calling-attendance.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    if(a == 1){
+        cout << "John";
+    }
+    else if(a == 2){
+        cout << "Tom";
+    }
+    else if(a == 3){
+        cout << "Paul";
+    }
+    else {
+        cout << "Vacncy";
+    }
+    return 0;
+}
