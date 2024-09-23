197594a2f5834de58f4063a55d7ee1741f5919a6
wnsgks3153 <72612538+wnsgks3153@users.noreply.github.com>
2024-09-23 11:49:26 +0900

    Title: 체질량지수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240923/체질량지수/bmi.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int j, w, b;
+    cin >> j >> w;
+
+    b = (10000 * w) / (h * h);
+
+    cout << b << endl;
+    if(b >= 25){
+        cout << "Obesity";
+    }
+    return 0;
+}
