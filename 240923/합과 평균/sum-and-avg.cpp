70d3613ecfcbf02963afb5e9f8dae69dc2844a27
wnsgks3153 <72612538+wnsgks3153@users.noreply.github.com>
2024-09-23 10:28:47 +0900

    Title: 합과 평균, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240923/합과 평균/sum-and-avg.cpp
+++ 240923/합과 평균/sum-and-avg.cpp
@@ -2,8 +2,10 @@
 using namespace std;
 
 int main() {
+    cout << fixed;
     int a, b;
     cin >> a >> b;
+    cout.precision(1);
     cout << a + b << " " << (double)(a + b) / 2; 
     return 0;
 }
